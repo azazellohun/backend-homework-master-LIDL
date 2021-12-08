cb936c440c19b807257aa9c50bad24434f8f0366
Gergely Lublovary <lublovarygergo@gmail.com>
2021-12-08 19:06:39 +0100

    add producer and consumer

--- src/importer.py
+++ src/importer.py
@@ -1,17 +1,70 @@
 import logging, logging.config, sys, yaml
 from kafka import KafkaProducer, KafkaConsumer
-from time import sleep
 from json import dumps, loads
-from os import path
+from pathlib import Path
+
+from reader.CsvReader import CsvReader, ConversionError
 
 
 def main(*args):
-    logger = logging.getLogger('importer')
-    logger.info('test')
+    topic_name = 'events'    
+    produce_records(topic_name, '/data')
+    consum_records(topic_name)
+
+
+def produce_records(topic_name, data_path):
+    """ Produce and log records to topic reading from data_path. 
+        :param topic_name: Name of the topic
+        :param data_path: Data path
+    """ 
+    logger = logging.getLogger('producer')
+    error_logger = logging.getLogger('error')
+
+    producer = KafkaProducer(bootstrap_servers=['kafka:9094'],
+                            api_version=(0,10,2),
+                            value_serializer=lambda x: dumps(x).encode('utf-8')
+                            )
+
+    reader = CsvReader(data_path)
+    
+    # Sending events to kafka
+    for record in reader:        
+        if isinstance(record, ConversionError):
+            error_logger.error(record.error_msg)
+            continue
+        
+        transaction_id, event_type,date, store_number, item_number, value = record
+        data = {
+            'transaction_id':transaction_id,
+            'event_type': event_type, 
+            'date': date.strftime('%Y-%m-%dT%H:%M:%S'), 
+            'store_number':store_number, 
+            'item_number':item_number, 
+            'value':value}
+        producer.send(topic_name, value=data)
+        logger.info(f'Data sent: {str(data)}')
+
+
+def consum_records(topic_name):
+    """ Consume and log records from topic. 
+        :param topic_name: Name of the topic
+    """ 
+
+    logger = logging.getLogger('consumer')
+    consumer = KafkaConsumer(topic_name,
+                             bootstrap_servers=['kafka:9092'],
+                             auto_offset_reset='earliest',
+                             enable_auto_commit=True,
+                             group_id='my-group',
+                             value_deserializer=lambda x: loads(x.decode('utf-8'))
+                            )
+
+    for consumer_record in consumer:
+        logger.info(f"Consumed: {consumer_record.value}")
 
 
 def set_up_logging():
-    logging_config_path = path.join(path.dirname(path.abspath(__file__)), 'log_conf.yaml')
+    logging_config_path = Path(__file__).parent.joinpath('log_conf.yaml')
 
     with open(logging_config_path, 'r') as stream:
         config = yaml.load(stream, Loader=yaml.FullLoader)
@@ -23,8 +76,9 @@ def set_up_logging():
 
 if __name__ == '__main__':    
     set_up_logging()
+    rootLogger = logging.getLogger()
 
     try:        
         main(sys.argv)
     except:
-        logging.critical('', exc_info=True)
+        rootLogger.critical('', exc_info=True)
